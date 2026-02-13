cacc2976a969ebd13ca7c33f2b601c920cfff57f
Sagar <ruparelia.sagar@gmail.com>
2026-02-12 21:06:28 -0500

    Initial commit

---
+++ src/main/java/com/chanakya/shl2/Shl2Application.java
@@ -0,0 +1,13 @@
+package com.chanakya.shl2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shl2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shl2Application.class, args);
+    }
+
+}
